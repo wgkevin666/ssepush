74fdb7b4f5ab181c84e4cf74a3594e6fda7d062b
KevinWang <kevinwang@osensetech.com>
2021-01-15 11:17:13 +0800

    測試3個index模仿user,確認是用type還是channel做區分

--- sse.py
+++ sse.py
@@ -8,22 +8,36 @@ app.register_blueprint(sse, url_prefix='/stream')
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/user2')
+def index1():
+    return render_template("index2.html")
+@app.route('/user3')
+def index2():
+    return render_template("index3.html")
 
-@app.route('/hello')
+@app.route('/hello', methods = ['POST'])
 def publish_hello():
-    sse.publish({"message": "hello kevin"}, type='greeting', retry=4500, channel="123")
+    json_data = request.json
+    message = json_data["message"]
+    sse.publish({"message": message}, type='greeting', retry=4500, channel="123")
     return "Message sent!"
 
-@app.route('/hello1')
+@app.route('/user', methods = ['POST'])
 def publish_hello1():
-    sse.publish({"message": "hello david"}, type='greeting', retry=4500, channel="456")
+    json_data = request.json
+    message = json_data["message"]
+    channel = json_data["user_id"]
+    # channel is userid
+    sse.publish({"message": message}, type='greeting2', channel="456")
     return "Message sent!!!"
 
-@app.route('/user', methods = ['POST','GET'])
-def publish_user():
-    print(request.method)
-    print(request.args)
-    message = request.args.get('message')
-    print(request.args.get("msg"))
+@app.route('/', methods = ['POST'])
+def publish_alluser():
+    # print(request.method)
+    # print(request.args)
+    # message = request.args.get('message')
+    # print(request.args.get("message"))
+    json_data = request.json
+    message = json_data["message"]
     sse.publish({"message": message }, type='postmessage')
     return "channel"
